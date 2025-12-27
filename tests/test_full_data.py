11b1f5cb3c21de6135cd18332707d5af379b8cc1
pranavsmoorthy <pranavsmoorthy@gmail.com>
2025-12-26 20:24:42 -0500

    added full flow to tests

--- tests/test_full_data.py
+++ tests/test_full_data.py
@@ -17,5 +17,23 @@ class TestFull(unittest.TestCase):
         log_debug("MP Data: " + str(filteredDataMP))
         log_debug("OQMD Data: " + str(filteredDataOQMD))
 
+    def test_full_flow(self):
+        import data.mp as mp
+        import data.oqmd as oqmd
+        from utils.debug import log_debug
+
+        formula = "CO923423843"
+
+        dataMP = mp.retrieveMPData(formula)
+        finalCandidate = None
+
+        if dataMP[0].get("dataFound"):
+            finalCandidate = mp.filter(dataMP)
+        else:
+            dataOQMD = oqmd.retrieveOQMDData(formula)
+            finalCandidate = oqmd.filter(dataOQMD)
+
+        log_debug("Final Candidate: " + str(finalCandidate))
+
 if __name__ == '__main__':
     unittest.main()
